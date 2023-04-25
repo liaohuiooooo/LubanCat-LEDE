595a0e2094202771fe050e8ec8bf8bb76337d722
骷髅头 <lasstp5011@gmail.com>
2023-04-25 13:35:29 +0800

    Update 6.1-lede.sh

--- scripts/6.1-lede.sh
+++ scripts/6.1-lede.sh
@@ -8,11 +8,12 @@
 #=================================================
 
 # mt7921
-rm -rf package/kernel/rtl8821cu
 rm -rf package/kernel/mac80211
+rm -rf package/kernel/rtl8821cu
 rm -rf package/network/services/hostapd
-svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mac80211
-svn export https://github.com/openwrt/openwrt/trunk/package/network/services/hostapd package/network/services/hostapd
+svn export https://github.com/DHDAXCW/openwrt-beta/trunk/package/kernel/mac80211 package/kernel/mac80211
+chmod a-x package/kernel/mac80211/Makefile
+svn export https://github.com/DHDAXCW/openwrt-beta/trunk/package/network/services/hostapd package/network/services/hostapd
 
 # alist
 git clone https://github.com/sbwml/luci-app-alist package/alist
