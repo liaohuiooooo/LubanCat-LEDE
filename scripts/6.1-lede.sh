b27d630100e55b86be17f71c59a38a795f88f9ef
骷髅头 <lasstp5011@gmail.com>
2023-05-06 19:44:09 +0800

    Update 6.1-lede.sh

--- scripts/6.1-lede.sh
+++ scripts/6.1-lede.sh
@@ -11,12 +11,8 @@
 rm -rf package/kernel/mac80211
 rm -rf package/kernel/rtl8821cu
 rm -rf package/network/services/hostapd
-#svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mac80211
-#svn export https://github.com/openwrt/openwrt/trunk/package/network/services/hostapd package/network/services/hostapd
-svn export https://github.com/DHDAXCW/wireless-program/trunk/hostapd package/network/services/hostapd
-svn export https://github.com/DHDAXCW/wireless-program/trunk/mac80211 package/kernel/mac80211
-rm -rf package/kernel/mt76
-svn export https://github.com/DHDAXCW/wireless-program/trunk/mt76-20230424 package/kernel/mt76
+svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mac80211
+svn export https://github.com/openwrt/openwrt/trunk/package/network/services/hostapd package/network/services/hostapd
 rm -rf package/kernel/mac80211/Makefile
 cp -f $GITHUB_WORKSPACE/Makefile package/kernel/mac80211/Makefile
 
