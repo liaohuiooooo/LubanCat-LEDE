b3f25da8dd5d7276c3c3c3855d14a10fa13d2e10
骷髅头 <lasstp5011@gmail.com>
2023-07-07 14:16:02 +0800

    Update 6.1-lede.sh

--- scripts/6.1-lede.sh
+++ scripts/6.1-lede.sh
@@ -13,12 +13,8 @@ cp -f $GITHUB_WORKSPACE/Makefile package/kernel/mac80211/Makefile
 
 # alist
 git clone https://github.com/sbwml/luci-app-alist package/alist
-pushd package/alist
-git reset --hard a5837a2014396cc330573c9d0b76863cf5651e07
-popd
-
 rm -rf feeds/packages/lang/golang
-svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
 
 # Clone community packages ok
 mkdir package/community
